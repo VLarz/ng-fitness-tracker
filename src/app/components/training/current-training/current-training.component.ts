0e995788fa28688ff3b8b9fa24d0da450778970f
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2021-02-13 19:19:04 +0800

    Added current training

--- src/app/components/training/current-training/current-training.component.ts
+++ src/app/components/training/current-training/current-training.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+  timer = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 20;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
+
+  onStop(): void {
+    clearInterval(this.timer);
   }
 
 }
